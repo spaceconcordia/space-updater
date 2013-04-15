71966f31675fdaed58d48c9908f32d26e8f796d3
Joseph-Antoine Martineau-Gagné <jamg85@ACER-4739.(none)>
2013-04-14 20:01:25 -0400

    Q6, Makefile

---
+++ Q6.cpp
@@ -0,0 +1,13 @@
+#include "Updater.h"
+int main(){
+    const char* path_new      = "/home/apps/new";
+    const char* path_current  = "/home/apps/current";
+    const char* path_old      = "/home/apps/old";
+    const char* path_rollback = "/home/apps/rollback";
+    Updater* updater = new Updater(path_new, path_current, path_old, path_rollback);
+    updater->StartUpdate();
+
+    delete updater;
+
+    return 0;
+}
