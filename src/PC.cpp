1855706cb1782a4993a59e5effd08d712c5c19d2
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2014-03-08 12:19:04 -0500

    define PATH for x86 usage

--- src/PC.cpp
+++ src/PC.cpp
@@ -1,10 +1,12 @@
 #include <Updater.h>
 int main(){
-    const char* path_new      = "/home/apps/new";
-    const char* path_current  = "/home/apps/current";
-    const char* path_old      = "/home/apps/old";
-    const char* path_rollback = "/home/apps/rollback";
-    const char* log_folder    = "/home/logs";
+    #undef __PATH__
+    #define __PATH__ "/home/jamg85/space"
+    const char* path_new      = __PATH__"/apps/new";
+    const char* path_current  = __PATH__"/apps/current";
+    const char* path_old      = __PATH__"/apps/old";
+    const char* path_rollback = __PATH__"/apps/rollback";
+    const char* log_folder    = __PATH__"/git/space-updater/logs";
 
     Updater* updater = new Updater(path_new, path_current, path_old, path_rollback, log_folder);
     
