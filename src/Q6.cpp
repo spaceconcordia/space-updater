2ea338a7f79976accbca2878eaeea55fba8dc9c0
Joseph-Antoine Martineau-Gagné <joseph-antoine@spaceconcordia.ca>
2014-04-12 14:52:27 -0400

    Q6.pc add SpaceDecl.h

--- src/Q6.cpp
+++ src/Q6.cpp
@@ -5,7 +5,7 @@ int main(){
     const char* path_current  = CS1_APPS"/current";
     const char* path_old      = CS1_APPS"/old";
     const char* path_rollback = CS1_APPS"/rollback";
-    const char* path_logs     = CS1_LOGS
+    const char* path_logs     = CS1_LOGS;
     
     Updater* updater = new Updater(path_new, path_current, path_old, path_rollback, path_logs);
 
